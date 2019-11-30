7ca312a6e40c43badd76d4f49ff79f817eb16266
charlesfox97 <charlesfox@protonmail.com>
2019-11-30 15:25:10 -0800

    VarianceConstraints.py

---
+++ VarianceConstraints.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 15:21:49 2019
+
+@author: Charl
+"""
+
+class VarianceConstraints:
+    def __init__(self,covar):
+        self.covar = covar
+        
